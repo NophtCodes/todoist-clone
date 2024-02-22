ae2d2b06ed5a2abfc69f75f7ccc9aa279aef1c0d
Nopht <kimartyom.dev@gmail.com>
2024-02-22 15:22:59 +0500

    hooks index.js conditions added

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+
+const firebaseConfig = firebase.initializeApp({
+    apiKey: "AIzaSyDi8c2eXJDhk8SOQTNQTAXNbxljABFAA0Q",
+    authDomain: "todoist-clone-4d690.firebaseapp.com",
+    databaseURL: "https://todoist-clone-4d690-default-rtdb.firebaseio.com",
+    projectId: "todoist-clone-4d690",
+    storageBucket: "todoist-clone-4d690.appspot.com",
+    messagingSenderId: "480454250750",
+    appId: "1:480454250750:web:b8f57bbc0b01c90d3cd5af"
+});
+
+export {
+    firebaseConfig as firebase
+};
