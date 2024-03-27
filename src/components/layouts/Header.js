845df70e35a07eb55c15f1d06a4c072b559ebbf0
Nopht <kimartyom.dev@gmail.com>
2024-03-27 18:07:49 +0500

    dark mode toggle

--- src/components/layouts/Header.js
+++ src/components/layouts/Header.js
@@ -1,6 +1,9 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { FaPizzaSlice } from 'react-icons/fa';
-const Header = () => {
+const Header = ({darkMode, setDarkMode}) => {
+    const [shouldShowMain, setShouldShowMain] = useState(false);
+    const [showQuickAddTask, setShowQuickAddTask] = useState(false);
+    
   return (
     <header className="header" data-testid="header">
         <nav>
@@ -10,10 +13,11 @@ const Header = () => {
             <div className="settings">
                 <ul>
                     <li data-testid="quick-add-task-action" className="settings__add">+</li>
-                    <li data-testid="dark-mode-action" className="settings__darkmode"><FaPizzaSlice/></li>
+                    <li data-testid="dark-mode-action" className="settings__darkmode" onClick={() => setDarkMode(!darkMode)}><FaPizzaSlice/></li>
                 </ul>
             </div>
         </nav>
+        
     </header>
   )
 };
